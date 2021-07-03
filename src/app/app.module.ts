b7a3365cdab61c8ad5b173d3312079f7c8470909
reinnovs <thaboroy@gmail.com>
2021-07-04 00:10:31 +0200

    Initial-Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,7 +6,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCardComponent
   ],
   imports: [
     BrowserModule
